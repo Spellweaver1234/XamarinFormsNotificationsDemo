1bb0ae78feb0db2cd6a4ae9cb1e7ecc3e4f18284
Arseniy (windows) <Arsenchozzz@mail.ru>
2023-06-22 14:01:02 +0300

    Add project files.

---
+++ XamarinFormsNotifications.Android/BootReceiver.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Content;
+
+namespace XamarinFormsNotifications.Droid
+{
+    [BroadcastReceiver(Enabled = true, Label = "Reboot complete receiver")]
+    [IntentFilter(new[] { Intent.ActionBootCompleted })]
+    public class BootReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            if (intent.Action == "android.intent.action.BOOT_COMPLETED")
+            {
+                // Recreate alarms
+            }
+        }
+    }
+}
---
+++ XamarinFormsNotifications.Android/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Xamarin.Forms;
+
+namespace XamarinFormsNotifications.Droid
+{
+    [Activity(Label = "LocalNotifications",
+        Icon = "@mipmap/icon",
+        Theme = "@style/MainTheme",
+        MainLauncher = true,
+        LaunchMode = LaunchMode.SingleTop,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+            CreateNotificationFromIntent(Intent);
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            CreateNotificationFromIntent(intent);
+        }
+
+        void CreateNotificationFromIntent(Intent intent)
+        {
+            if (intent?.Extras != null)
+            {
+                string title = intent.GetStringExtra(AndroidNotificationManager.TitleKey);
+                string message = intent.GetStringExtra(AndroidNotificationManager.MessageKey);
+
+                DependencyService.Get<INotificationManager>().ReceiveNotification(title, message);
+            }
+        }
+    }
+}
